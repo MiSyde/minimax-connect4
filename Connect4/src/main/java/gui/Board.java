3699f81cc9ff7470a66b9a13c6620459ef24f43a
Battai Máté <battaimate@gmail.com>
2025-10-11 18:31:31 +0200

    Board looks presentable now

--- Connect4/src/main/java/gui/Board.java
+++ Connect4/src/main/java/gui/Board.java
@@ -4,18 +4,24 @@ import core.Game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 
 public class Board {
 
     public static void main(String[] args) {
         JFrame window = new JFrame("Connect 4");
         JFrame.setDefaultLookAndFeelDecorated(true);
-        window.setIconImage(new ImageIcon(Board.class.getResource("/gui/icon.png")).getImage());
+        URL icon = Board.class.getResource("/gui/icon.png");
+        if(icon != null){
+            window.setIconImage(new ImageIcon(icon).getImage());
+        } else{
+            System.err.println("⚠️ Icon not found: /gui/icon.png");
+        }
         CoinPanel panel = new CoinPanel();
         window.setContentPane(panel);
         Game a = new Game();
         panel.addMouseListener(new BoardClickListener(panel, a));
-        window.setSize(700,600);
+        window.setSize(717,640);
         window.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
--- Connect4/src/main/java/gui/CoinPanel.java
+++ Connect4/src/main/java/gui/CoinPanel.java
@@ -16,10 +16,51 @@ public class CoinPanel extends JPanel {
         this.repaint();
     }
 
+    private void boardLineSetup(Graphics g){
+        // Vertical
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(4));
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        Color l = new Color(96, 95, 95);
+        g.setColor(l);
+        g.drawLine(0, 0, 0, 600);
+        g.drawLine(100, 0, 100, 600);
+        g.drawLine(200, 0, 200, 600);
+        g.drawLine(300, 0, 300, 600);
+        g.drawLine(400, 0, 400, 600);
+        g.drawLine(500, 0, 500, 600);
+        g.drawLine(600, 0, 600, 600);
+        g.drawLine(700, 0, 700, 600);
+        // Horizontal
+        g.drawLine(0, 100, 700, 100);
+        g.drawLine(0, 200, 700, 200);
+        g.drawLine(0, 300, 700, 300);
+        g.drawLine(0, 400, 700, 400);
+        g.drawLine(0, 500, 700, 500);
+        g.drawLine(0, 600, 700, 600);
+    }
+
+    private void boardCircleSetup(Graphics g) {
+        Color circles = new Color(92, 92, 92);
+        g.setColor(circles);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        for(int i = 0; i<7; i++){
+            for(int j = 0; j<6; j++){
+                g.drawOval(i*100+10, j*100+10, 80, 80);
+                g.fillOval(i*100+10, j*100+10, 80, 80);
+            }
+        }
+    }
+
+
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        setBackground(Color.GRAY);
+        Color bg = new Color(142, 142, 142);
+        setBackground(bg);
+        boardCircleSetup(g);
+        boardLineSetup(g);
         for(Coin coin : coins){
             coin.draw(g);
         }
